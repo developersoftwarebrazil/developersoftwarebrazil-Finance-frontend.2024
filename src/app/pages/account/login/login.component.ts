1193fe0b8e2fcea1eb428aa563c90f06775601e9
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-02-12 17:53:58 -0300

    Adicionado o typescript para a página de login

--- src/app/pages/account/login/login.component.ts
+++ src/app/pages/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  // variaveis
+  loginForm: FormGroup;
+  inputFocused: boolean = false;
+
+  constructor(
+    private router: Router,
+    public formBuilder: FormBuilder,
+  ) { }
+
+  // metodos
+  // executado ao iniciar a página
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group
+      ({
+        email: ["", [Validators.required, Validators.email]],
+        password: ['', [Validators.required]]
+      })
+  }
+
+   // Método para remover a borda do input quando ocorre o evento focus
+   onFocus() {
+    this.inputFocused = true;
+  }
+  onBlur() {
+    this.inputFocused = false;
+  }
+
+  // pega os dados do usuário
+  get formData() {
+    return this, this.loginForm.controls;
+  }
+
+  // metodo executado apos o dados serem preenchido no formulário
+  userLogin() {
+    alert('ok')
+  }
 }
