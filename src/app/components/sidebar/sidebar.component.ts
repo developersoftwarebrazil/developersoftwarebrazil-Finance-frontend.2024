af806f994dcb5886b7edae49f08bc0a9ef3480c2
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-02-22 02:54:18 -0300

    Finalizado os métod para selecionar o menu no sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
+import { SystemComponent } from './../../pages/system/system.component';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuService } from '../../services/menuservice';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,14 +11,53 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class SidebarComponent implements OnInit {
   @Input() sidebarActive = false;
   @Input() isSidebarOpen = false;
- @Output() toggleSidebar = new EventEmitter();
+  @Output() toggleSidebar = new EventEmitter();
 
 
- constructor(){}
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    public menuService: MenuService
+  ) { }
+  ngOnInit(): void { }
 
+  //método para abrir e fechar o sidebar
   toggleSidebarOpen() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  menuSelect(menu: number) {
+    switch (menu) {
+      case 1:
+        this.router.navigate(['/dashboard']);
+        break;
+
+      case 2:
+        this.router.navigate(['/system']);
+        break;
+
+      case 3:
+        this.router.navigate(['/category']);
+        break;
+
+      case 4:
+        this.router.navigate(['/expense']);
+        break;
+
+      case 5:
+        this.router.navigate(['/user']);
+        break;
+
+      case 100:
+        localStorage.clear();
+        this.router.navigate(['/login']);
+        break;
+
+
+      default:
+        break;
+    }
+    // guarda o nome da variável escolhida no menu
+    this.menuService.menuSelected = menu;
   }
 
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MenuService } from '../../services/menuservice';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @Input() sidebarActive = false;
   @Input() isSidebarActive = false;
+
+  constructor(public menuService: MenuService) { }
+  ngOnInit(): void {
+    this.menuService.menuSelected = 1;
+  }
 }
