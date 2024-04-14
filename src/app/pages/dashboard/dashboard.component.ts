38a77019e0a8c36d85358a8a4cac9f0b79e39563
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-04-14 16:47:24 -0300

    Implementado o card de Investimentos.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ExpenseService } from '../../services/expense.service';
+import { IncomeService } from '../../services/income.service';
 import { MenuService } from '../../services/menuservice';
 import { ThemeService } from '../../services/theme.service';
 
@@ -22,20 +23,29 @@ export class DashboardComponent implements OnInit {
     private themeService: ThemeService,
     public menuService: MenuService,
     public expenseService: ExpenseService,
+    public incomeService: IncomeService,
     public authService: AuthService) { }
 
   ngOnInit(): void {
     this.systemTheme = this.themeService.detectSystemTheme();
     this.menuService.menuSelected = 1;
     this.loadExpenseGraph();
+    this.loadIncomeGraph();
   }
 
   loadExpenseGraph() {
     this.expenseService.LoadExpenseGraph(this.authService.getUserEmail())
-      .subscribe((response: any) => {
+      .subscribe((responseExpense: any) => {
         debugger
-        this.objectToExpenseGraph = response;
-        console.log(response);
+        this.objectToExpenseGraph = responseExpense;
+        console.log(responseExpense);
+      }, (error) => console.error(error), () => { })
+  }
+  loadIncomeGraph() {
+    this.incomeService.LoadIncomeGraph(this.authService.getUserEmail())
+      .subscribe((responseIncome: any) => {
+        debugger
+        this.objectToIncomeGraph = responseIncome;
       }, (error) => console.error(error), () => { })
   }
 }
